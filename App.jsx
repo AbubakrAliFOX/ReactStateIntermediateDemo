b1b189c6b384319b4a162277b2d8a90adc933ada
Abubakr Ali <44189367+AbubakrAliFOX@users.noreply.github.com>
2024-02-01 20:57:47 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Counter from './Counter'
+import EmojiClicker from './Emojis'
+import ScoreKeeper from './ScoreKeeper'
+
+function App() {
+
+  return (
+    <>
+      {/* <h1>Hi</h1> */}
+      {/* <Counter /> */}
+      {/* <EmojiClicker/> */}
+      <ScoreKeeper numPlayer = {10}/>
+    </>
+  )
+}
+
+export default App
